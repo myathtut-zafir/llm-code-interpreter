a68c7a2ad574f409c6dd91e224df5536e1b1bac4
myathtut-zafir <aa.htut@gmail.com>
2024-12-14 15:16:26 +0700

    feat: csv agent

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 from langchain import hub
 from langchain_openai import ChatOpenAI
-from langchain.agents import create_react_agent,AgentExecutor
+from langchain.agents import create_react_agent, AgentExecutor
 from langchain_experimental.tools import PythonREPLTool
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
 
 load_dotenv()
+
 
 def main():
     instructions = """You are an agent designed to write and execute python code to answer questions.
@@ -14,21 +16,38 @@ def main():
     You might know the answer without running any code, but you should still run the code to get the answer.
     If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
     """
-    base_prompt=hub.pull("langchain-ai/react-agent-template")
-    prompt=base_prompt.partial(instructions=instructions)
+    # base_prompt = hub.pull("langchain-ai/react-agent-template")
+    # prompt = base_prompt.partial(instructions=instructions)
 
-    tools=[PythonREPLTool()]
-    agent = create_react_agent(
-        prompt=prompt,
-        llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
-        tools=tools,
+    tools = [PythonREPLTool()]
+    # agent = create_react_agent(
+    #     prompt=prompt,
+    #     llm=ChatOpenAI(temperature=0, model="gpt-4-turbo"),
+    #     tools=tools,
+    # )
+    # agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+    # agent_executor.invoke(
+    #     input={
+    #         "input": """generate and save in current working directory 15 QRcodes
+    #                             that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+    #     }
+    # )
+    csv_agent = create_csv_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4"),
+        path="episode_info.csv",
+        verbose=True,
+        allow_dangerous_code=True
     )
-    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-    agent_executor.invoke(
+    csv_agent.invoke(
+        input={"input": "how many columns are there in file episode_info.csv"}
+    )
+
+    csv_agent.invoke(
         input={
-            "input": """generate and save in current working directory 15 QRcodes
-                                that point to www.udemy.com/course/langchain, you have qrcode package installed already"""
+            "input": "print the seasons by ascending order of the number of episodes they have"
         }
     )
-if __name__=="__main__":
+
+
+if __name__ == "__main__":
     main()
